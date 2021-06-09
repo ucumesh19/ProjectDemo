7e1261c54b853d87f7cc602a2615fe9beab5deaf
ucumesh19 <85158092+ucumesh19@users.noreply.github.com>
2021-06-09 23:29:26 +0530

    Modify

---
+++ src/common/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Icons, nf, hpx, wpx, hp, wp } from '../constants/constants';
+
+const Header = ({ name }) => {
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.head1}>{name}</Text>
+            <Text style={styles.head2}>Clear All</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 12,
+        justifyContent: "space-between",
+        backgroundColor: "#FFFFFF",
+        //  marginHorizontal: wpx(22),
+        borderBottomWidth: 1,
+        borderBottomColor: "#C9C9C9"
+    },
+    head1: {
+        fontSize: nf(20),
+        color: "#606060",
+    },
+    head2: {
+        fontSize: nf(12),
+        color: "#606060",
+    }
+})
+
+export default Header;
---
+++ src/components/AuthScreen/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Home = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.textStyle}>Welcome to Home Page</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1
+    },
+    textStyle: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "blue"
+
+
+    }
+})
+
+export default Home;
